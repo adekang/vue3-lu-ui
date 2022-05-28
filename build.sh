c4bfcbec6c6b08204e258ea711bafe420762f0bc
adekang <adekang@163.com>
2022-05-28 23:09:39 +0800

    core: script

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
+cd packages/utils &&
+pnpm build &&
+nrm use npm &&
+pnpm publish &&
+cd ../../ &&
 pnpm build &&
 pnpm publish -F lu-ui &&
-pnpm clean
+pnpm clean &&
+pnpm publish &&
+nrm use taobao
 
+echo "打包并上传成功！！"
